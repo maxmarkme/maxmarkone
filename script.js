42d55aece8a4b743ad837cc610c4775a2211aa66
maxmarkme <142175107+maxmarkme@users.noreply.github.com>
2023-08-14 00:14:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -66,10 +66,34 @@ function storeCurrentDrawing() {
 
 function toggleDistortion() {
     isDistorted = !isDistorted;
+    drawControlPoints();
 }
 
 function distortStart(event) {
-    // ... [Rest of the distortion logic]
+    const x = event.clientX - canvas.offsetLeft;
+    const y = event.clientY - canvas.offsetTop;
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        const row = Math.floor(i / 4 / canvas.width);
+        const col = (i / 4) % canvas.width;
+        const dx = col - x;
+        const dy = row - y;
+        const dist = Math.sqrt(dx * dx + dy * dy);
+
+        if (dist < 100) {
+            const factor = (100 - dist) * 0.2;
+            const newx = col + dx * factor;
+            const newy = row + dy * factor;
+            const newPixel = (Math.floor(newy) * canvas.width + Math.floor(newx)) * 4;
+            data[i] = data[newPixel];
+            data[i + 1] = data[newPixel + 1];
+            data[i + 2] = data[newPixel + 2];
+        }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
 }
 
 function getClickedControlPoint(x, y) {
