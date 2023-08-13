ff9a576ff94b6d67caf4f1258147b59f4f597acd
maxmarkme <142175107+maxmarkme@users.noreply.github.com>
2023-08-13 23:35:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('artCanvas');
 const ctx = canvas.getContext('2d');
 let drawing = false;
+let isDistorted = false;
+let storedImageData;
 
 canvas.addEventListener('mousedown', () => {
     drawing = true;
@@ -8,6 +10,7 @@ canvas.addEventListener('mousedown', () => {
 
 canvas.addEventListener('mouseup', () => {
     drawing = false;
+    storeCurrentDrawing();
     ctx.beginPath();
 });
 
@@ -23,4 +26,24 @@ function draw(event) {
     ctx.stroke();
     ctx.beginPath();
     ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+}
+
+function storeCurrentDrawing() {
+    storedImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+}
+
+function toggleDistortion() {
+    if (isDistorted) {
+        ctx.putImageData(storedImageData, 0, 0);
+    } else {
+        const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        // Apply distortion effect (this is a basic example, you can customize as needed)
+        for (let i = 0; i < imgData.data.length; i += 4) {
+            imgData.data[i] = 255 - imgData.data[i];
+            imgData.data[i + 1] = 255 - imgData.data[i + 1];
+            imgData.data[i + 2] = 255 - imgData.data[i + 2];
+        }
+        ctx.putImageData(imgData, 0, 0);
+    }
+    isDistorted = !isDistorted;
 }
