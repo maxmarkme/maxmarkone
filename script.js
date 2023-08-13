c9aa765f7c10c6386e325bc6ea5d73b68554a973
maxmarkme <142175107+maxmarkme@users.noreply.github.com>
2023-08-13 23:27:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+const canvas = document.getElementById('artCanvas');
+const ctx = canvas.getContext('2d');
+let drawing = false;
+let isDistorted = false;
+let storedImageData;
+
+canvas.addEventListener('mousedown', () => {
+    drawing = true;
+});
+
+canvas.addEventListener('mouseup', () => {
+    drawing = false;
+    storeCurrentDrawing();
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
+
+function draw(event) {
+    if (!drawing) return;
+    ctx.lineWidth = 50; 
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'black';
+
+    ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+}
+
+function storeCurrentDrawing() {
+    storedImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+}
+
+function toggleDistortion() {
+    if (isDistorted) {
+        ctx.putImageData(storedImageData, 0, 0);
+    } else {
+        const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        // Apply distortion effect (this is a basic example, you can customize as needed)
+        for (let i = 0; i < imgData.data.length; i += 4) {
+            imgData.data[i] = 255 - imgData.data[i];
+            imgData.data[i + 1] = 255 - imgData.data[i + 1];
+            imgData.data[i + 2] = 255 - imgData.data[i + 2];
+        }
+        ctx.putImageData(imgData, 0, 0);
+    }
+    isDistorted = !isDistorted;
+}
+
 function saveDrawing() {
     const dataURL = canvas.toDataURL();
     const link = document.createElement('a');
@@ -22,5 +72,17 @@ function promptSocialMediaShare() {
     if (userResponse) {
         alert("Redirecting to social media sharing page...");
         // Implement logic to share on social media and award the "Spread the Love" badge or discount
+    }
+}
+
+function postToSocialMedia() {
+    alert('Social media posting to be implemented!');
+}
+
+function offerNFT() {
+    const userResponse = confirm("Would you like to purchase a 3D NFT version of your artwork?");
+    if (userResponse) {
+        alert("Redirecting to NFT purchase page...");
+        // Redirect to NFT purchase page or implement NFT purchase logic here
     }
 }
