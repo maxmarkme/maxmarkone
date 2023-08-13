1158a6972baabb617224d84ec39a47743169ada9
maxmarkme <142175107+maxmarkme@users.noreply.github.com>
2023-08-13 23:24:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,26 @@
-const canvas = document.getElementById('artCanvas');
-const ctx = canvas.getContext('2d');
-let drawing = false;
-let isDistorted = false;
-
-canvas.addEventListener('mousedown', () => {
-    drawing = true;
-});
-
-canvas.addEventListener('mouseup', () => {
-    drawing = false;
-    ctx.beginPath();
-});
-
-canvas.addEventListener('mousemove', draw);
-
-function draw(event) {
-    if (!drawing) return;
-    ctx.lineWidth = 50; // This sets the thickness of the line
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = 'black';
-
-    ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
-}
-
-function toggleDistortion() {
-    if (isDistorted) {
-        ctx.globalCompositeOperation = 'source-over'; // Default value
-    } else {
-        ctx.globalCompositeOperation = 'difference'; // This will create a "negative" effect on overlap
-    }
-    isDistorted = !isDistorted;
-}
-
 function saveDrawing() {
-    alert('Save functionality to be implemented!');
+    const dataURL = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = dataURL;
+    link.download = 'artwork.png';
+    link.click();
+    offerPhysicalArtwork();
+    offerNFT();
 }
 
-function postToSocialMedia() {
-    alert('Social media posting to be implemented!');
+function offerPhysicalArtwork() {
+    const userResponse = confirm("Would you like to receive a physical version of your artwork?");
+    if (userResponse) {
+        alert("Redirecting to shipping details page...");
+        // Redirect to a secure page where they can enter their shipping details
+    }
+    promptSocialMediaShare();
+}
+
+function promptSocialMediaShare() {
+    const userResponse = confirm("Would you like to share your artwork on social media and spread the love?");
+    if (userResponse) {
+        alert("Redirecting to social media sharing page...");
+        // Implement logic to share on social media and award the "Spread the Love" badge or discount
+    }
 }
