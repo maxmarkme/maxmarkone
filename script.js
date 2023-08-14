fc4acd50af217c58c3e8354df13f1789cc3e1968
maxmarkme <142175107+maxmarkme@users.noreply.github.com>
2023-08-14 02:28:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const canvas = document.getElementById('artCanvas');
 const ctx = canvas.getContext('2d');
 let drawing = false;
-let isDistorted = false;
 let storedImageData;
 
 let draggingControlPoint = null;
 let controlPoints = [
+    { x: 0, y: 0 },
+    { x: canvas.width, y: 0 },
+    { x: canvas.width, y: canvas.height },
+    { x: 0, y: canvas.height }
+];
+
+let originalPoints = [
     { x: 0, y: 0 },
     { x: canvas.width, y: 0 },
     { x: canvas.width, y: canvas.height },
@@ -19,19 +25,17 @@ canvas.addEventListener('mousedown', (e) => {
     
     if (clickedPoint) {
         draggingControlPoint = clickedPoint;
-    } else if (isDistorted) {
-        distortStart(e);
     } else {
         drawing = true;
+        ctx.beginPath();
     }
 });
 
 canvas.addEventListener('mouseup', () => {
     drawing = false;
     draggingControlPoint = null;
-    storeCurrentDrawing();
-    ctx.beginPath();
     drawControlPoints();
+    applyDistortion();
 });
 
 canvas.addEventListener('mousemove', (e) => {
@@ -48,87 +52,12 @@ canvas.addEventListener('mousemove', (e) => {
 });
 
 function draw(event) {
-    if (!drawing) return;
-    ctx.lineWidth = 50; 
+    ctx.lineWidth = 5; 
     ctx.lineCap = 'round';
     ctx.strokeStyle = 'black';
 
     ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
     ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
-    drawControlPoints();
-}
-
-function storeCurrentDrawing() {
-    storedImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-}
-
-function toggleDistortion() {
-    isDistorted = !isDistorted;
-    drawControlPoints();
-}
-
-function distortStart(event) {
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
-    const output = ctx.createImageData(canvas.width, canvas.height);
-    const outputData = output.data;
-
-    for (let y = 0; y < canvas.height; y++) {
-        for (let x = 0; x < canvas.width; x++) {
-            const i = (y * canvas.width + x) * 4;
-            
-            const relPos = getRelativePosition(x, y, controlPoints);
-            
-            const srcX = relPos.x * canvas.width;
-            const srcY = relPos.y * canvas.height;
-            const srcPixel = bilinearInterpolation(data, srcX, srcY, canvas.width, canvas.height);
-            
-            outputData[i] = srcPixel[0];
-            outputData[i + 1] = srcPixel[1];
-            outputData[i + 2] = srcPixel[2];
-            outputData[i + 3] = 255; // alpha
-        }
-    }
-
-    ctx.putImageData(output, 0, 0);
-}
-
-function getRelativePosition(x, y, points) {
-    return { x: x / canvas.width, y: y / canvas.height };
-}
-
-function bilinearInterpolation(data, x, y, width, height) {
-    const x1 = Math.floor(x);
-    const y1 = Math.floor(y);
-    const x2 = x1 + 1;
-    const y2 = y1 + 1;
-
-    const f11 = getPixel(data, x1, y1, width);
-    const f12 = getPixel(data, x1, y2, width);
-    const f21 = getPixel(data, x2, y1, width);
-    const f22 = getPixel(data, x2, y2, width);
-
-    const r1 = ((x2 - x) / (x2 - x1)) * f11[0] + ((x - x1) / (x2 - x1)) * f21[0];
-    const r2 = ((x2 - x) / (x2 - x1)) * f12[0] + ((x - x1) / (x2 - x1)) * f22[0];
-
-    const g1 = ((x2 - x) / (x2 - x1)) * f11[1] + ((x - x1) / (x2 - x1)) * f21[1];
-    const g2 = ((x2 - x) / (x2 - x1)) * f12[1] + ((x - x1) / (x2 - x1)) * f22[1];
-
-    const b1 = ((x2 - x) / (x2 - x1)) * f11[2] + ((x - x1) / (x2 - x1)) * f21[2];
-    const b2 = ((x2 - x) / (x2 - x1)) * f12[2] + ((x - x1) / (x2 - x1)) * f22[2];
-
-    const r = ((y2 - y) / (y2 - y1)) * r1 + ((y - y1) / (y2 - y1)) * r2;
-    const g = ((y2 - y) / (y2 - y1)) * g1 + ((y - y1) / (y2 - y1)) * g2;
-    const b = ((y2 - y) / (y2 - y1)) * b1 + ((y - y1) / (y2 - y1)) * b2;
-
-    return [r, g, b];
-}
-
-function getPixel(data, x, y, width) {
-    const i = (y * width + x) * 4;
-    return [data[i], data[i + 1], data[i + 2]];
 }
 
 function getClickedControlPoint(x, y) {
@@ -150,4 +79,38 @@ function drawControlPoints() {
     }
 }
 
+function applyDistortion() {
+    // Simple linear interpolation for distortion
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+
+    for (let y = 0; y < canvas.height; y++) {
+        for (let x = 0; x < canvas.width; x++) {
+            const i = (y * canvas.width + x) * 4;
+
+            const fx = (x / canvas.width);
+            const fy = (y / canvas.height);
+
+            const distortedX = lerp(originalPoints[0].x, originalPoints[1].x, fx);
+            const distortedY = lerp(originalPoints[0].y, originalPoints[3].y, fy);
+
+            const srcX = distortedX + (controlPoints[1].x - controlPoints[0].x) * fx + (controlPoints[3].x - controlPoints[0].x) * fy;
+            const srcY = distortedY + (controlPoints[3].y - controlPoints[0].y) * fy + (controlPoints[1].y - controlPoints[0].y) * fx;
+
+            const srcPixel = (Math.floor(srcY) * canvas.width + Math.floor(srcX)) * 4;
+
+            data[i] = data[srcPixel];
+            data[i + 1] = data[srcPixel + 1];
+            data[i + 2] = data[srcPixel + 2];
+        }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+}
+
+function lerp(start, end, t) {
+    return start * (1 - t) + end * t;
+}
+
+// Initially draw the control points
 drawControlPoints();
