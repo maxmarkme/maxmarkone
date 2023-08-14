8f3a7711bc30ed2229094e98ac5776c3906449f1
maxmarkme <142175107+maxmarkme@users.noreply.github.com>
2023-08-14 14:21:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-paper.setup(document.getElementById('artCanvas'));
+const canvas = document.getElementById('artCanvas');
+const ctx = canvas.getContext('2d');
+let drawing = false;
 
-let path;
+canvas.addEventListener('mousedown', (e) => {
+    drawing = true;
+    ctx.beginPath();
+    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+});
 
-function onMouseDown(event) {
-    path = new paper.Path();
-    path.strokeColor = 'black';
-    path.strokeWidth = 50;
-    path.add(event.point);
-}
+canvas.addEventListener('mouseup', () => {
+    drawing = false;
+});
 
-function onMouseDrag(event) {
-    path.add(event.point);
-}
-
-paper.view.draw();
+canvas.addEventListener('mousemove', (e) => {
+    if (drawing) {
+        ctx.lineWidth = 100;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'black';
+        ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    }
+});
