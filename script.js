600c5a3192747e0f9626bcdd958cbe801ecf0d0a
maxmarkme <142175107+maxmarkme@users.noreply.github.com>
2023-08-14 14:06:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,33 @@
-let canvas = new fabric.Canvas('artCanvas');
+const app = new PIXI.Application({ width: 500, height: 500, backgroundColor: 0xFFFFFF });
+document.getElementById('pixiCanvas').appendChild(app.view);
+
 let isDrawing = false;
+let graphics = new PIXI.Graphics();
+app.stage.addChild(graphics);
 
-canvas.on('mouse:down', function(options) {
+app.view.addEventListener('mousedown', startDrawing);
+app.view.addEventListener('mousemove', draw);
+app.view.addEventListener('mouseup', endDrawing);
+
+function startDrawing(e) {
     isDrawing = true;
-    canvas.isDrawingMode = true;
-    canvas.freeDrawingBrush.width = 50;
-    canvas.freeDrawingBrush.color = "#000000";
-});
+    graphics.lineStyle(50, 0x000000, 1);
+    graphics.moveTo(e.offsetX, e.offsetY);
+}
 
-canvas.on('mouse:up', function(options) {
+function draw(e) {
+    if (!isDrawing) return;
+    graphics.lineTo(e.offsetX, e.offsetY);
+    graphics.moveTo(e.offsetX, e.offsetY);
+    graphics.endFill();
+}
+
+function endDrawing() {
     isDrawing = false;
-    canvas.isDrawingMode = false;
-    applyDistortion();
-});
-
-// Add control points
-let controlPoint1 = new fabric.Circle({ radius: 5, fill: 'red', left: 10, top: 10, hasBorders: false, hasControls: false });
-let controlPoint2 = new fabric.Circle({ radius: 5, fill: 'red', left: canvas.width - 10, top: 10, hasBorders: false, hasControls: false });
-let controlPoint3 = new fabric.Circle({ radius: 5, fill: 'red', left: canvas.width - 10, top: canvas.height - 10, hasBorders: false, hasControls: false });
-let controlPoint4 = new fabric.Circle({ radius: 5, fill: 'red', left: 10, top: canvas.height - 10, hasBorders: false, hasControls: false });
-
-canvas.add(controlPoint1, controlPoint2, controlPoint3, controlPoint4);
-
-// Logic to distort the drawing based on control points
-function applyDistortion() {
-    // This is a basic distortion effect. It's not perfect, but it's a start.
-    let scaleX = controlPoint2.left / canvas.width;
-    let scaleY = controlPoint3.top / canvas.height;
-    canvas.getObjects().forEach(obj => {
-        if (obj !== controlPoint1 && obj !== controlPoint2 && obj !== controlPoint3 && obj !== controlPoint4) {
-            obj.scaleX = scaleX;
-            obj.scaleY = scaleY;
-            obj.setCoords();
-        }
-    });
-    canvas.renderAll();
 }
 
-function saveDrawing() {
-    // Placeholder for save drawing logic
-}
+// Placeholder functions
+function saveDrawing() {}
+function postToSocialMedia() {}
 
-function postToSocialMedia() {
-    // Placeholder for post to social media logic
-}
+// TODO: Implement the perspective distortion effect using PIXI's capabilities.
